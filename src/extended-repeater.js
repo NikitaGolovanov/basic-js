c02b67ff2504965e954d180e219c0ff02d8fb36e
nikitagolovanov <119881857+NikitaGolovanov@users.noreply.github.com>
2023-04-18 02:42:04 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -4,7 +4,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Create a repeating string based on the given parameters
  *  
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} opt opt object 
  * @return {String} repeating string
  * 
  *
@@ -15,10 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // for 
+function repeater(str,opt) {
+  var repeatTimes = 1
+  if (opt.hasOwnProperty('repeatTimes')) repeatTimes = opt.repeatTimes
+  var separator = '+'
+  if (opt.hasOwnProperty('separator')) separator = opt.separator
+  var addition = ''
+  if (opt.hasOwnProperty('addition')) addition = opt.addition
+  var additionRepeatTimes = 1
+  if (opt.hasOwnProperty('additionRepeatTimes')) additionRepeatTimes = opt.additionRepeatTimes
+  var additionSeparator = '|'
+  if (opt.hasOwnProperty('additionSeparator')) additionSeparator = opt.additionSeparator
+  var fin = ''
+  
+  for (let i = 0; i<repeatTimes;i++){
+    fin+=str 
+    for (let j = 0; j<additionRepeatTimes; j++) {
+      fin+=addition
+      if (j != additionRepeatTimes-1) fin+=additionSeparator
+    }
+    if (i != repeatTimes-1) fin+=separator
+  }
+  return fin
 }
+
 
 module.exports = {
   repeater
